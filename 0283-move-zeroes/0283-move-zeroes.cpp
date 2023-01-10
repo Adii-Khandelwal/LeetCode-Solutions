17857ee89b04de07d603693cc71eaea61c719b0b
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-01-10 15:11:31 +0530

    Time: 18 ms (97.35%), Space: 19.2 MB (85.20%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int i;
+        int n=nums.size();
+        ////////////////////////////////////////////////
+//         vector<int> vec;
+//                                 // BRUTE FORCE
+//         for(i=0;i<n;i++)
+//         {
+//             if(nums[i]!=0)
+//                 vec.push_back(nums[i]);
+//         }
+        
+//         for(i=0;i<vec.size();i++)
+//         {
+//             nums[i]=vec[i];
+//         }
+//         for(i=vec.size();i<nums.size();i++)
+//         {
+//             nums[i]=0;
+//         }
+     ////////////////////////////////////////////////////////
+        int zero=0; // OPTIMISED APPROACH
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[zero]);
+                zero++;
+            }
+        }
+        
+        
+    }
+};
