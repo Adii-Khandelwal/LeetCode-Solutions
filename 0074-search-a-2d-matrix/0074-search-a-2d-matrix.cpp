13d5407637b6e583db5db9f458c49257844a786c
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-26 14:45:26 +0530

    Time: 5 ms (57.40%), Space: 9.4 MB (95.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,36 +4,58 @@ public:
         
         int m=matrix.size();
         int n=matrix[0].size();
-        int j=m-1;
+//         int j=m-1;
     
         
-        for(int i=0;i<m;i++)
-        {
-             if(matrix[i][0]==target)
-                return true;
-        }
-        for(int i=0;i<m-1;i++)
-        {
+//         for(int i=0;i<m;i++)
+//         {
+//              if(matrix[i][0]==target)
+//                 return true;
+//         }
+//         for(int i=0;i<m-1;i++)
+//         {
            
-            if(matrix[i][0]<target && target<matrix[i+1][0])
-            {
-              j=i;  
-                break;
-            }
-        }
-        int f=0;
-        cout << j<< endl;
-        for(int i=1;i<n;i++)
-        {
+//             if(matrix[i][0]<target && target<matrix[i+1][0])
+//             {
+//               j=i;  
+//                 break;
+//             }
+//         }
+//         int f=0;
+//         cout << j<< endl;
+//         for(int i=1;i<n;i++)
+//         {
             
            
-            if(matrix[j][i]==target)
-            { 
-                f=2;
+//             if(matrix[j][i]==target)
+//             { 
+//                 f=2;
+//                 return true;
+//             }
+//              // cout << f;
+//         }
+//         return false;
+        
+        
+        
+        int l=0;
+        int r=m*n-1;
+        
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            
+            if(matrix[mid/n][mid%n]<target)
+                l=mid+1;
+            else if(matrix[mid/n][mid%n]>target)
+                r=mid-1;
+            else
                 return true;
-            }
-             // cout << f;
         }
         return false;
+        
+        
+        
+        
     }
 };
