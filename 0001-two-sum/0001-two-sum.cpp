01953df3e716c472bd024fc259369f2ba74a2b00
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-26 16:58:55 +0530

    Time: 778 ms (5.41%), Space: 10.3 MB (66.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,47 +3,20 @@ class Solution
     public:
         vector<int> twoSum(vector<int> &nums, int target)
         {
-           	//          int i,j;
-           	//         vector<int> ans;
-           	//         for(i=0;i < nums.size();i++)
-           	//         {         	for(j=i+1;j < nums.size();j++)
-           	//             {
-           	//                 if(nums[i]+nums[j]==target)
-           	//                 {
-           	//                     ans.push_back(i);
-           	//                     ans.push_back(j);
-           	//                     goto final;
-           	//             }
-
-           	//     }}
-           	//         final: return ans;
-
-            unordered_map<int, int> mpp;
-            int i;
-            for (i = 0; i < nums.size(); i++)
+         vector<int> ans;
+            int n=nums.size();
+            for(int i=0;i<n;i++)
             {
-                mpp.emplace(nums[i], i);
-            }
-            vector<int> vec;
-           	// int first=-1;
-           	// auto second=-1;
-
-            for (i = 0; i < nums.size(); i++)
-            {
-               	// first=i;
-                if (mpp.find(target - nums[i]) != mpp.end())
+                for(int j=i+1;j<n;j++)
                 {
-                    if (i != mpp[target - nums[i]])
+                    if(nums[i]==target-nums[j])
                     {
-                        vec.emplace_back(i);
-                        vec.emplace_back(mpp[(target) - nums[i]]);
-
-                        goto final;
+                        ans.push_back(i);
+                        ans.push_back(j);
                     }
                 }
             }
-           	// return vec;
-
-            final: return vec;
+        
+        return ans;
         }
 };
