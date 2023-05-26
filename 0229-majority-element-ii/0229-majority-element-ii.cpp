be7107932a6dda2fa92ba227cad64bb435c465f3
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-26 15:52:18 +0530

    Time: 16 ms (48.69%), Space: 15.9 MB (77.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,28 +3,91 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
         
-        vector<int> ans;
-        map<int,int> mpp;
+//         vector<int> ans;
+//         map<int,int> mpp;
         
-        for(int i=0;i<n;i++)
-        {
-            mpp[nums[i]]++;
-        }
+//         for(int i=0;i<n;i++)
+//         {
+//             mpp[nums[i]]++;
+//         }
         
-        for(int i=0;i<n;i++)
-        {
-            if(mpp[nums[i]]>(n/3))
-            {
+//         for(int i=0;i<n;i++)
+//         {
+//             if(mpp[nums[i]]>(n/3))
+//             {
                 
-                // auto it=find(ans.begin(),ans.end(),nums[i]);
-                // if(it==ans.end());
-                ans.push_back(nums[i]);
-            mpp[nums[i]]=0;
+//                 // auto it=find(ans.begin(),ans.end(),nums[i]);
+//                 // if(it==ans.end());
+//                 ans.push_back(nums[i]);
+//             mpp[nums[i]]=0;
             
             
             
             
-            } }
-        return ans;
+//             } }
+//         return ans;
+    
+    int cnt1=0;
+        int cnt2=0;
+        int n1=-1;
+        int n2=-1;
+        
+        for(int  i=0;i<n;i++)
+        {
+            if(nums[i]==n1)
+                cnt1++;
+            else if(nums[i]==n2)
+                cnt2++;
+            else if(cnt1==0)
+            {
+                n1=nums[i];
+                cnt1=1;}
+            else if (cnt2==0)
+            {
+                n2=nums[i];
+                cnt2=1;
+            }
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }}
+            
+            vector<int> ans;
+            cnt1=cnt2=0;
+            
+            int s=nums.size()/3;
+            for(int i=0;i<n;i++)
+            {
+                if(nums[i]==n1)
+                    cnt1++;
+                else if(nums[i]==n2)
+                    cnt2++;
+                
+            }
+            if(cnt1>s)
+                ans.push_back(n1);
+            if(cnt2>s)
+                ans.push_back(n2);
+            
+            return ans;
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 };
