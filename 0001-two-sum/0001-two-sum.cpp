d8b43320c3d22bd6da3b46001b6372c64b4b0824
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-26 17:18:42 +0530

    Time: 21 ms (51.89%), Space: 12.3 MB (5.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,20 +3,48 @@ class Solution
     public:
         vector<int> twoSum(vector<int> &nums, int target)
         {
-         vector<int> ans;
-            int n=nums.size();
-            for(int i=0;i<n;i++)
+//          vector<int> ans;
+//             int n=nums.size();
+//             for(int i=0;i<n;i++)
+//             {
+//                 for(int j=i+1;j<n;j++)
+//                 {
+//                     if(nums[i]==target-nums[j])
+//                     {
+//                         ans.push_back(i);
+//                         ans.push_back(j);
+//                     }
+//                 }
+//             }
+        
+//         return ans;
+            int i;
+            map<int,int> mpp;
+            for (i = 0; i < nums.size(); i++)
             {
-                for(int j=i+1;j<n;j++)
+                mpp.emplace(nums[i], i);
+            }
+            vector<int> vec;
+           	// int first=-1;
+           	// auto second=-1;
+
+            for (i = 0; i < nums.size(); i++)
+            {
+               	// first=i;
+                if (mpp.find(target - nums[i]) != mpp.end())
                 {
-                    if(nums[i]==target-nums[j])
+                    if (i != mpp[target - nums[i]])
                     {
-                        ans.push_back(i);
-                        ans.push_back(j);
+                        vec.emplace_back(i);
+                        vec.emplace_back(mpp[(target) - nums[i]]);
+
+                        goto final;
                     }
                 }
             }
-        
-        return ans;
+           	// return vec;
+
+            final: return vec;
+            
         }
 };
