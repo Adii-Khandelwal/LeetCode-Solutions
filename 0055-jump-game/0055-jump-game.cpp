fdfa82d5ee374cd3662dca17b014293b326e18e3
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-26 11:04:30 +0530

    Time: 528 ms (17.85%), Space: 50.6 MB (13.47%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -26,10 +26,44 @@ public:
     
     bool canJump(vector<int>& nums) {
         
-        vector<int>dp(nums.size(),-1);
-        return jump(nums,0,dp);
+        // vector<int>dp(nums.size(),-1);
+        // return jump(nums,0,dp);
+        int n=nums.size();
         
         
+        vector<int> dp(nums.size(),-1);
+        
+        dp[n-1]=1;
+        
+        for(int ind=n-2;ind>=0;ind--)
+        {
+            if(nums[ind]==0)
+            { dp[ind]=0;
+            continue;
+            }
+        
+        
+        
+       
+            int reach=ind+nums[ind];
+            int f=0;
+            for(int j=ind+1;j<=reach;j++)
+        {
+            if(dp[j])
+            {dp[ind]=true;
+            f=1;
+             break;
+            }
+        }
+            if(f==1)
+                continue;
+            
+        dp[ind]=false;
+        
+        
+        }
+
+        return dp[0];
         
     }
 };
