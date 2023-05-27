5415f0550174764bf370d59b08717a296e4624b4
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-27 16:05:08 +0530

    Time: 188 ms (5.17%), Space: 128.1 MB (5.32%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // int n = gas.size();
+        // for(int i = 0; i < n; i++){
+        //     int totalFuel = 0;
+        //     int Count = 0, j = i;
+        //     while(Count < n){
+        //         totalFuel += gas[j % n] - cost[j % n];
+        //         if(totalFuel < 0) break; // whenever we reach -ve
+        //         Count++;
+        //         j++;
+        //     }
+        //     if(Count == n && totalFuel >= 0) return i; // cover all the stops & our fuel left is 0 or more than that
+        // }
+        // return -1;
+        
+        
+        
+        
+        
+        //////////////////////////
         int n=gas.size();
         
         int asum=0;
