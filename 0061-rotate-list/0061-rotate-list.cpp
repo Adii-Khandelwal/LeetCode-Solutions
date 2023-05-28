837edb4c34ad2b451e7383bc510368230d64d120
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-05-28 12:31:49 +0530

    Time: 10 ms (41.73%), Space: 11.6 MB (99.42%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,31 +12,69 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
        
+//         if(!head)
+//         {
+//             return NULL;
+//         }
+        
+//         vector<int> arr;
+//         ListNode *p=head;
+//         while(p)
+//         {
+//             arr.push_back(p->val);
+//             p=p->next;
+//         }
+       
+//          int n=arr.size();
+//         if(k>n)
+//         {
+//             k=k%n;
+//         }
+//         p=head;
+//         for(int i=0;i<n;i++)
+//         {
+//             p->val=arr[(n-k+i)%n];
+//             p=p->next;
+//         }
+//         return head;
+        
         if(!head)
-        {
             return NULL;
+        
+        ListNode *tail;
+        
+        int cnt=1;
+        tail=head;
+        while(tail->next)
+        {
+            cnt++;
+            tail=tail->next;
+        }
+        tail->next=head;
+        
+        int hind= cnt-k%cnt;
+        
+        for(int i=0;i<hind;i++)
+        {
+            tail=tail->next;
+            
         }
         
-        vector<int> arr;
-        ListNode *p=head;
-        while(p)
-        {
-            arr.push_back(p->val);
-            p=p->next;
-        }
-       
-         int n=arr.size();
-        if(k>n)
-        {
-            k=k%n;
-        }
-        p=head;
-        for(int i=0;i<n;i++)
-        {
-            p->val=arr[(n-k+i)%n];
-            p=p->next;
-        }
+        head=tail->next;
+        tail->next=NULL;
         return head;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
