0fd7ae92c48b2ce01a7db3e01798306cb0723d94
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-02 18:28:33 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (51.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,40 +2,63 @@ class Solution {
 public:
     
     
-    int f(int ind,vector<int>& nums,vector<int> &dp)
-    {
-        if(ind==0)
-            return dp[ind]= nums[0];
-        if(ind <0)
-            return 0;
+//     int f(int ind,vector<int>& nums,vector<int> &dp)
+//     {
+//         if(ind==0)
+//             return dp[ind]= nums[0];
+//         if(ind <0)
+//             return 0;
         
-        if(dp[ind]!=-1)return dp[ind];
+//         if(dp[ind]!=-1)return dp[ind];
         
-       int notpick=0+f(ind-1,nums,dp);
-      // int pick=INT_MIN;
+//        int notpick=0+f(ind-1,nums,dp);
+//       // int pick=INT_MIN;
       
-        int pick=nums[ind]+f(ind-2,nums,dp);
+//         int pick=nums[ind]+f(ind-2,nums,dp);
             
-        return dp[ind]= max(pick,notpick);
-    }
+//         return dp[ind]= max(pick,notpick);
+//     }
     
     
     int rob(vector<int>& nums) {
             int n=nums.size();
-        vector<int> dp(n+1,0);
+        // vector<int> dp(n+1,0);
         
-        dp[0]=nums[0];
+//         dp[0]=nums[0];
+//         for(int ind=1;ind<n;ind++)
+//         {
+//               int notpick=0+dp[ind-1];
+//       int pick=nums[ind];
+      
+//             if(ind>=2)
+//         pick+=dp[ind-2];
+            
+//      dp[ind]= max(pick,notpick);
+//         }
+//         
+        // return dp[n-1];
+        
+        int prev2=0;
+        int prev=nums[0];
+        // int curr=nums[0];
+        
+        
         for(int ind=1;ind<n;ind++)
         {
-              int notpick=0+dp[ind-1];
+              int notpick=   prev;
       int pick=nums[ind];
       
             if(ind>=2)
-        pick+=dp[ind-2];
-            
-     dp[ind]= max(pick,notpick);
+        pick+=prev2;
+          
+           int curr=max(pick,notpick);
+     prev2=prev;
+        prev=curr;
         }
-        return dp[n-1];
+        
+        return prev;
+        
+        
         
     }
 };
