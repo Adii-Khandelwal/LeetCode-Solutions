a5ab0022503b45eb50907c8329f0d7d5c0b7a04f
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-03 16:04:26 +0530

    Time: 85 ms (11.47%), Space: 11 MB (82.42%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -33,7 +33,41 @@ public:
         
         
         
-         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+//          vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        
+//         for(int ind=n-1;ind>=0;ind--)
+//         {
+//             for(int buy=0;buy<2;buy++)
+//             {
+//                 for(int cap=1;cap<=k;cap++)
+//                 {
+                 
+                     
+//         if(buy)
+//         {
+//             dp[ind][buy][cap]=max( -prices[ind]+dp[ind+1][0][cap],0 + dp[ind+1][1][cap]);
+//         }
+//         else
+//         {
+//             dp[ind][buy][cap]=max( prices[ind]+dp[ind+1][1][cap-1],0 + dp[ind+1][0][cap] );
+//         }
+//        // dp[ind][buy][cap]= profit;
+//                 }
+//             }
+//         }
+        
+//         return dp[0][1][k];
+        
+        
+        
+          // int n=prices.size();
+        
+    vector<vector<int>> after(2,vector<int>(k+1,0));
+    vector<vector<int>> curr(2,vector<int>(k+1,0));
+        
+       
+        
+       
         
         for(int ind=n-1;ind>=0;ind--)
         {
@@ -45,19 +79,20 @@ public:
                      
         if(buy)
         {
-            dp[ind][buy][cap]=max( -prices[ind]+dp[ind+1][0][cap],0 + dp[ind+1][1][cap]);
+            curr[buy][cap]=max( -prices[ind]+after[0][cap],0 + after[1][cap]);
         }
         else
         {
-            dp[ind][buy][cap]=max( prices[ind]+dp[ind+1][1][cap-1],0 + dp[ind+1][0][cap] );
+            curr[buy][cap]=max( prices[ind]+after[1][cap-1],0 + after[0][cap] );
         }
+                    
+                    after=curr;
        // dp[ind][buy][cap]= profit;
                 }
             }
         }
         
-        return dp[0][1][k];
-        
+        return curr[1][k];
         
         
         
