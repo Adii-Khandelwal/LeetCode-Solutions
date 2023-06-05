b2b5b11939a9e674da87c838837e3229593e1b0c
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-05 10:51:57 +0530

    Time: 8 ms (76.63%), Space: 13.6 MB (43.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,71 @@
 class Solution {
 public:
-    int binarySearch(vector<int> v,int l,int h,int key)
+    int f(vector<int>& nums, int target)
     {
+        int first=-1;
+        int l=0;
+        int h=nums.size()-1;
+     
         while(l<=h)
         {
-            int mid=l+(h-l)/2;
-            if(key==v[mid])
-                return mid;
-            else if(key < v[mid])
+            int mid=(l+h)/2;
+            
+            if(nums[mid]==target)
+            {
+                first=mid;
+                h=mid-1;
+            }
+            else if(nums[mid]>target)
                 h=mid-1;
             else
                 l=mid+1;
         }
-        return -1;
+        return first;
+    }  
+        
+        int la(vector<int>& nums, int target)
+    {
+        int last=-1;
+        int l=0;
+        int h=nums.size()-1;
+     
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            
+            if(nums[mid]==target)
+            {
+                last=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]>target)
+                h=mid-1;
+            else
+                l=mid+1;
+        }
+            return last;
     }
     
     
     vector<int> searchRange(vector<int>& nums, int target) {
        
-       int first=-1;
-        int last=-1;
+//        int first=-1;
+//         int last=-1;
         
-        int n = nums.size();
+//         int n = nums.size();
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==target)
-            {
-                if(first==-1)
-                    first=i;
-                last=i;
-            }
-        }
-        return {first,last};
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 if(first==-1)
+//                     first=i;
+//                 last=i;
+//             }
+//         }
+//         return {first,last};
+       
+        return {f(nums,target),la(nums,target)};
+        
     }
 };
