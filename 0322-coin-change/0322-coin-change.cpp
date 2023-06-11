ff9ef80f243972cbc6a770e9e4bebafd329907ab
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-11 15:57:34 +0530

    Time: 108 ms (62.91%), Space: 45.6 MB (27.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int f(int ind,int n, int amount,vector<int> &coins,vector<vector<int>> &dp)
+    int f(int ind,int amount,vector<int> &coins,vector<vector<int>> &dp)
     {
-        if(amount==0)
-            return 0;
-        if(ind>n)
-            return 1e9;
+       if(ind==0)
+       {
+           if(amount%coins[0]==0)
+               return amount/coins[0];
+           return 1e9;
+       }
         
         if(dp[ind][amount]!=-1)return dp[ind][amount];
         
-        int nottake=f(ind+1,n,amount,coins,dp);
+        int nottake=f(ind-1,amount,coins,dp);
         int take=1e9;
         if(coins[ind]<=amount)
-            take=1+f(ind,n,amount-coins[ind],coins,dp);
+            take=1+f(ind,amount-coins[ind],coins,dp);
         
         return dp[ind][amount]= min(take,nottake);
     }
@@ -25,9 +27,31 @@ public:
         
         int n=coins.size();
         vector<vector<int>>dp(n,(vector<int> (amount+1,-1)));
-        int ans= f(0,n-1,amount,coins,dp);
+        int ans= f(n-1,amount,coins,dp);
         if(ans>=1e9)
             return -1;
         return ans;
+        
+//         int n=coins.size();
+//         vector<vector<int>>dp(n,(vector<int> (amount+1,0)));
+    
+//         for(int i=0;i<n;i++)
+//         {
+//             dp[i][0]=0;
+//         }
+        
+        
+//         for(int ind=0;ind<n;ind++)
+//             for(int x=amount;x>=0;x--)
+//             {  int nottake=dp[ind+1][x];
+//         int take=1e9;
+//         if(coins[ind]<=amount)
+//             take=1+dp[ind][x-coins[ind]];
+        
+//         dp[ind][amount]= min(take,nottake);
+//             }
+//         return dp[n-1][0];
+    
+    
     }
 };
