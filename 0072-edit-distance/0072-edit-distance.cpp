50f5c74bf968a01a1ccacd2f8aa8fa737b1d2104
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-12 19:51:14 +0530

    Time: 18 ms (50.41%), Space: 9.3 MB (20.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int f(int i, int j,string &s1,string &s2,vector<vector<int>>&dp)
     {
-        if(i<0)
-            return  j+1;
-        if(j<0)
-            return  i+1;
+        // if(i<0)
+        if(i==0)
+            return  j;
+        // if(j<0)
+            if(j==0)
+        return  i;
         
         if(dp[i][j]!=-1)return dp[i][j];
         
-        if(s1[i]==s2[j])
+        if(s1[i-1]==s2[j-1])
             return  dp[i][j]= f(i-1,j-1,s1,s2,dp);
         return dp[i][j]= 1+min({f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)});
     }
@@ -20,8 +22,8 @@ public:
         
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
         
     }
 };
