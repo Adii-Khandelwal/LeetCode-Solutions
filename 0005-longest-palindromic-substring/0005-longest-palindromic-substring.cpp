a04a62726dfc894d540d28e7e5556b86f01fa219
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-12 14:42:09 +0530

    Time: 1325 ms (12.64%), Space: 7 MB (75.71%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool check(string &s,int i,int j)
+    {
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+                return false;
+        i++;
+            j--;
+        }
+        return true;
+    }
+    
+    
+    
+    string longestPalindrome(string s) {
+        
+        int n=s.size();
+        
+        int maxln=0;
+        int stind=0;
+        for(int i=0;i<n;i++)
+        {
+            
+            for(int j=i;j<n;j++)
+            {
+                if(check(s,i,j))
+                {
+                    if(j-i+1>maxln)
+                    {
+                        maxln=j-i+1;
+                        stind=i;
+                    }
+                    
+                }
+            }
+        }
+        
+        return s.substr(stind,maxln);
+        
+        
+        
+    }
+};
