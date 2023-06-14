57c16b64d7a2f14e420bd807b64f606dde942064
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-14 12:01:14 +0530

    Time: 3 ms (68.69%), Space: 7.7 MB (49.50%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,35 +1,56 @@
 class Solution {
 public:
-    void recurse(vector<int> &nums, vector<int >&temp,vector<vector<int>> & ans, vector<int> &path)
+//     void recurse(vector<int> &nums, vector<int >&temp,vector<vector<int>> & ans, vector<int> &path)
+//     {
+//         if(temp.size()==nums.size())
+//         {
+//             ans.push_back(temp);
+//             return ;
+//         }
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(path[i]!=1)
+//             {
+//                 path[i]=1;
+//                  temp.push_back(nums[i]);
+//                 recurse(nums,temp,ans,path);
+//                 path[i]=0;
+//                 temp.pop_back();
+//             }
+//         }
+    
+    
+    void f(int ind,vector<int> &nums,vector<vector<int>> &ans)
     {
-        if(temp.size()==nums.size())
+        if(ind==nums.size())
         {
-            ans.push_back(temp);
-            return ;
+            ans.push_back(nums);
         }
         
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(path[i]!=1)
-            {
-                path[i]=1;
-                 temp.push_back(nums[i]);
-                recurse(nums,temp,ans,path);
-                path[i]=0;
-                temp.pop_back();
-            }
+            swap(nums[ind],nums[i]);
+            f(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
-        
-        
-        
     }
+        
+        
+    
     
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        vector<int> path(nums.size(),0);
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         vector<int> path(nums.size(),0);
         
-     recurse(nums,temp,ans,path);
+//      recurse(nums,temp,ans,path);
+//         return ans;
+        
+           vector<vector<int>> ans;
+        f(0,nums,ans);
         return ans;
+        
+        
     }
 };
