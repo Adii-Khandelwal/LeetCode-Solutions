cc817d42ba0cc0a7406b214dc64b5ed8d9bad436
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-14 11:50:01 +0530

    Time: 7 ms (26.64%), Space: 7.7 MB (49.50%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void recurse(vector<int> &nums, vector<int >&temp,vector<vector<int>> & ans, vector<int> &path)
+    {
+        if(temp.size()==nums.size())
+        {
+            ans.push_back(temp);
+            return ;
+        }
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(path[i]!=1)
+            {
+                path[i]=1;
+                 temp.push_back(nums[i]);
+                recurse(nums,temp,ans,path);
+                path[i]=0;
+                temp.pop_back();
+            }
+        }
+        
+        
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        vector<int> path(nums.size(),0);
+        
+     recurse(nums,temp,ans,path);
+        return ans;
+    }
+};
