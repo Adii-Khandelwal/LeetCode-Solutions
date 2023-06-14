e398651a4bfb449fed756affe26fa286b518e72f
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-14 15:18:11 +0530

    Time: 185 ms (56.92%), Space: 48.2 MB (71.46%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool dfs(int node,vector<vector<int>>& graph, vector<int> &vis,vector<int >& pathvis,vector<int> &check)
+    {
+        vis[node]=1;
+        pathvis[node]=1;
+        check[node]=0;
+        
+        for(auto it: graph[node])
+        {
+            if(!vis[it])
+            {
+               if( dfs(it,graph,vis,pathvis,check))
+               {
+                   check[node]=0;
+                   return true;
+               }
+            }
+                
+            else if(pathvis[it])
+            {
+                check[node]=0;
+                return true;
+            }
+        }
+        check[node]=1;
+        pathvis[node]=0;
+        return false;
+    }
+    
+    
+    
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+            int n =graph.size();
+        // int vis[n]={0};
+        vector<int>vis(n,0);
+        // int pathvis[n]={0};
+        vector<int> pathvis(n,0);
+        // int check[n]={0};
+vector<int> check(n,0);        
+        vector<int> safenodes;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                dfs( i,graph,vis,pathvis,check);
+            }
+            
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(check[i])
+                safenodes.push_back(i);
+        }
+        sort(safenodes.begin(),safenodes.end());
+        return safenodes;
+        
+    }
+};
