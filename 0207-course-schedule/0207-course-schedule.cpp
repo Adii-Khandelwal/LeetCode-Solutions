ab4c62db8992aee76e54a46368deafbd35c5ddfa
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-14 16:16:11 +0530

    Time: 19 ms (90.81%), Space: 13.8 MB (61.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> adj[n];
         for(auto it:prerequisites)
         {
-            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }    
         
         vector<int> indegree(n,0);
