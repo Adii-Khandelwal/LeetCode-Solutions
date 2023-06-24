21608bdaca786f59c1d722bc66008975efbe7a00
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-24 16:36:40 +0530

    Time: 13 ms (15.96%), Space: 10.5 MB (36.28%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+      
+        stack<string> s;
+        string res;
+        vector<string> p;
+
+        size_t start = 0;
+        size_t end = path.find('/');
+
+        while (end != string::npos) {
+            string segment = path.substr(start, end - start);
+
+            if (!segment.empty()) {
+                if (segment == "..") {
+                    if (!s.empty()) {
+                        s.pop();
+                    }
+                } else if (segment != ".") {
+                    s.push(segment);
+                }
+            }
+
+            start = end + 1;
+            end = path.find('/', start);
+        }
+
+        string segment = path.substr(start);
+        if (!segment.empty()) {
+            if (segment == "..") {
+                if (!s.empty()) {
+                    s.pop();
+                }
+            } else if (segment != ".") {
+                s.push(segment);
+            }
+        }
+
+        if (s.empty()) {
+            return "/";
+        }
+
+        while (!s.empty()) {
+            res = '/' + s.top() + res;
+            s.pop();
+        }
+
+        return res;
+
+
+   
+    }
+};
