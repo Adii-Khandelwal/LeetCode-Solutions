f0811f18863b90acf83d62d9d75aff4ba6c94891
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-24 19:49:03 +0530

    Time: 3 ms (86.11%), Space: 6.7 MB (98.09%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,25 +1,56 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int maxi=0;
+//         int maxi=0;
         
-        stack<int>st;
-        st.push(-1);
+//         stack<int>st;
+//         st.push(-1);
         
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(')
-            {
-                st.push(i);
+//         for(int i=0;i<s.size();i++)
+//         {
+//             if(s[i]=='(')
+//             {
+//                 st.push(i);
+//             }
+//             else 
+//             {
+//                 st.pop();
+//                 if(st.empty())
+//                     st.push(i);
+//                 maxi=max(maxi,i-st.top());
+//             }
+//         }
+//         return maxi;
+        
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
             }
-            else 
-            {
-                st.pop();
-                if(st.empty())
-                    st.push(i);
-                maxi=max(maxi,i-st.top());
+            if (left == right) {
+                maxlength = max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
             }
         }
-        return maxi;
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+        
+        
+        
     }
 };
