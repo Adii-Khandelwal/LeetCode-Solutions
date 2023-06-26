95f73afe113700d7fd37a0f37d4cb6667726f991
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-06-26 15:10:57 +0530

    Time: 15 ms (74.95%), Space: 13.5 MB (14.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,30 +2,49 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-    vector<vector<int>> temp=matrix;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    for(int k=0;k<n;k++)
-                    {
-                        temp[i][k]=0;
-                    }
-                    for(int k=0;k<m;k++)
-                        temp[k][j]=0;
+//     vector<vector<int>> temp=matrix;
+//         int m=matrix.size();
+//         int n=matrix[0].size();
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     for(int k=0;k<n;k++)
+//                     {
+//                         temp[i][k]=0;
+//                     }
+//                     for(int k=0;k<m;k++)
+//                         temp[k][j]=0;
+//                 }
+//             }
+//         }
+        
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 matrix[i][j]=temp[i][j];
+//             }
+//         }
+          int n = matrix.size();
+        int m = matrix[0].size();
+        unordered_set<int> setRows; 
+        unordered_set<int> setColumns; 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    setRows.insert(i);
+                    setColumns.insert(j);
                 }
             }
         }
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                matrix[i][j]=temp[i][j];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(setRows.count(i) > 0 || setColumns.count(j) > 0){
+                    matrix[i][j] = 0;
+                }
             }
         }
         
