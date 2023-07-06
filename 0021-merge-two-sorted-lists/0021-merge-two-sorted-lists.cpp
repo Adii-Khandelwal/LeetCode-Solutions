8c741ffbdacad7784ff2ad80325df43afdb020bd
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-07-06 12:01:03 +0530

    Time: 8 ms (71.33%), Space: 14.7 MB (57.41%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,55 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-        ListNode *third;
-        ListNode *last;
-        
-        if(!list1 || !list2)
-            if(!list1)
-                return list2;
-        else return list1;
-        
-        if(list1->val < list2->val)
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+       // if list1 happen to be NULL
+		// we will simply return list2.
+		if(l1 == NULL)
         {
-            third=last=list1;
-            list1=list1->next;
-            last->next=NULL;
-        }
-        else
+			return l2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(l2 == NULL)
         {
-            third=last=list2;
-            list2=list2->next;
-            last->next=NULL;
-
-        }
-        
-        while(list1 && list2)
-        if(list1->val > list2->val)
+			return l1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
         {
-            last->next=list2;
-            last=list2;
-            list2=list2->next;
-            last->next=NULL;
-            
-        }
-        else
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
         {
-            last->next=list1;
-            last=list1;
-            list1=list1->next;
-            last->next=NULL;
- 
-        }
-        
-        if(list1)
-        {
-            last->next=list1;
-            
-        }else{
-            last->next=list2;
-        }
-        return third;
-    }
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
+    
 };
