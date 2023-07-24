3af3ded962e6a3da9e32a577b09fbe5e98e634a3
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-07-24 10:22:45 +0530

    Time: 4 ms (40.18%), Space: 6.1 MB (28.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -8,22 +8,15 @@ public:
             n=abs(n);
             x=1/x;
         }
-        
-        
         if(n==0)
             return 1;
-        
-        
-        
         if(n==1)
             return x;
         
-    
-            // x=x*(x);
-            if(n%2==0)
+        if(n%2==0)
            return myPow(x*x,n/2);
-            else
-                return x*myPow(x*x,n/2);
+        else
+            return x*myPow(x*x,n/2);
         
         // return ans;
     }
