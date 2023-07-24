67a4e5cd22e8b176839bc92aea73b0702df3eadc
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-07-24 18:08:59 +0530

    Time: 8 ms (40.23%), Space: 7.2 MB (31.84%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    bool f(string &s,string &p,int i,int j)
+    // O(3^(max(m,n)) and space complexity is O(max(m,n)
+   
+    bool f(string &s,string &p,int i,int j,vector<vector<int>>&dp)
     {
         if(j==p.size())
         {
             return i==s.size();
         }
         
+        if(dp[i][j]!=-1)return dp[i][j];
+        
         bool firstM= (i<s.size() and (s[i] == p[j] || p[j]=='.'));
-                      
+            bool ans=0;          
         if(j+1<p.length() && p[j+1]=='*')
         {
-            return (f(s,p,i,j+2)|| (firstM && f(s,p,i+1,j) ));
+           ans=  (f(s,p,i,j+2,dp)|| (firstM && f(s,p,i+1,j,dp) ));
         }
         else
         {
-            return (firstM && f(s,p,i+1,j+1));
+            ans= (firstM && f(s,p,i+1,j+1,dp));
         }
-        
+        dp[i][j]=ans;
+        return ans;
         
     }
     
     
     
     bool isMatch(string s, string p) {
-        return f(s,p,0,0);
+       vector<vector<int>> dp(s.length()+1,vector<int>(p.length(),-1));
+        return f(s,p,0,0,dp);
     }
 };
