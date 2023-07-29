acbfc28291e3d236bf66bcb4ce11c4ad1fe5922f
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-07-29 22:36:34 +0530

    Time: 16 ms (78.73%), Space: 11.4 MB (40.39%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,27 +10,65 @@
  */
 class Solution {
 public:
+     int getLengthOfLinkedList(ListNode *head)
+    {
+        ListNode *ptr = head;
+        int cnt=0;
+        while(ptr)
+        {
+            cnt++;
+            ptr=ptr->next;
+        }
+        return cnt;
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *cursor=head;
-        for(int i=0;i<k;i++)
+//         ListNode *cursor=head;
+//         for(int i=0;i<k;i++)
+//         {
+//             if(cursor==NULL)return head;
+//             cursor=cursor->next;
+//         }
+        
+//         ListNode* curr=head,*prev=NULL,*nxt=NULL;
+//         for(int i=0;i<k;i++)
+//         {
+//             nxt=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=nxt;
+//         }
+//         head->next=reverseKGroup(curr,k);
+//         return prev;
+       if(!head)
+            return NULL;
+    
+        int len = getLengthOfLinkedList(head);
+        if(len<k)
+            return head;
+        
+        int numberOfGroupsToReverse = len/k;
+        
+        ListNode *dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode *start = dummyNode;
+
+        ListNode *pre,*remaining,*nxt;
+        for(int i=0;i<numberOfGroupsToReverse;i++)
         {
-            if(cursor==NULL)return head;
-            cursor=cursor->next;
+            pre = NULL;
+            remaining = head;
+            for(int j=0;j<k;j++)
+            {
+                nxt = head->next;
+                head->next = pre;
+                pre=head;
+                head=nxt;
+            }
+            start->next = pre;
+            remaining->next = head;
+            start = remaining;
         }
         
-        ListNode* curr=head,*prev=NULL,*nxt=NULL;
-        for(int i=0;i<k;i++)
-        {
-            nxt=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nxt;
-        }
-        head->next=reverseKGroup(curr,k);
-        return prev;
-        
-        
-        
-        
+        return dummyNode->next;
     }
 };
