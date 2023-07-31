abe584f5b6096e63dda3f9141dab7da2f732d1ba
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-07-31 17:44:36 +0530

    Time: 104 ms (94.98%), Space: 61.2 MB (40.59%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -20,24 +20,49 @@ public:
 //         }
 //         return ans;
       
-        map<int,int> mpp;
-        int x;
-        for(int i=0;i<nums.size();i++)
-        {
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==2)
+//         map<int,int> mpp;
+//         int x;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             mpp[nums[i]]++;
+//             if(mpp[nums[i]]==2)
             
-            {
-                x=nums[i];
-                return x;
-            }
+//             {
+//                 x=nums[i];
+//                 return x;
+//             }
                 
-        }
+//         }
         
        
         
-    return -1;    
-        
+//     return -1;    
+         int len = nums.size();
+        int low = 1;
+        int high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+        /*// Lets say there is an separate array containing numbers 
+        [1, 2, 3, 4, .... N]
+// we will apply binary search on "this" array! & shrink in the direction of repeated number
+// Let's say n=10 and I select mid=5. Then I count all the numbers in the array which are less than equal mid.
+// If there are more than 5 numbers that are less than 5, one of them has occurred more than once. So I shrink the search space from [1 10] to [1 5].
+// Otherwise the duplicate number is in the second half so for the next step the search space would be [6 10].*/
         
     }
 };
