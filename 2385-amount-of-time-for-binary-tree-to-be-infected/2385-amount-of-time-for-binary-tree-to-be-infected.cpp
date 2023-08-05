f8d89b2fd90708fd70200f3adaba8567ee0b0186
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-08-05 11:54:31 +0530

    Time: 464 ms (71.33%), Space: 273.4 MB (9.31%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,64 +1,111 @@
 class Solution {
-     TreeNode * f(TreeNode * root ,int t,map<TreeNode * ,TreeNode * > &mpp){
-        queue<TreeNode *>q;
-        q.push(root);
-        TreeNode * res;
-        while(!q.empty()){
-            TreeNode * node = q.front();
-            q.pop();
-            if(node->val == t){
-                res = node;
-            }
-            if(node->left){
-                mpp[node->left]=node;
-                q.push(node->left);
-            }
-            if(node->right){
-                mpp[node->right]=node;
-                q.push(node->right);
+    //  TreeNode * f(TreeNode * root ,int t,map<TreeNode * ,TreeNode * > &mpp){
+    //     queue<TreeNode *>q;
+    //     q.push(root);
+    //     TreeNode * res;
+    //     while(!q.empty()){
+    //         TreeNode * node = q.front();
+    //         q.pop();
+    //         if(node->val == t){
+    //             res = node;
+    //         }
+    //         if(node->left){
+    //             mpp[node->left]=node;
+    //             q.push(node->left);
+    //         }
+    //         if(node->right){
+    //             mpp[node->right]=node;
+    //             q.push(node->right);
+    //         }
+    //     }
+    //     return res;
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+public:
+     int ans = 0;
+    void traversal(TreeNode * root,vector<int> graph[],int parent){
+        if(root == NULL){
+            return;
+        }
+
+        if(parent!=-1){
+            graph[root->val].push_back(parent);
+            graph[parent].push_back(root->val);
+        }
+
+        traversal(root->left,graph,root->val);
+        traversal(root->right,graph,root->val);
+        return;
+    } 
+    
+    void dfs(int curr , vector<int> graph[] , vector<int>&depth , int parent){
+        if(parent!=-1) depth[curr] = depth[parent]+1;
+
+        ans = max(ans,depth[curr]);
+        for(auto x : graph[curr]){
+            if(x!=parent){
+                dfs(x,graph,depth,curr);
             }
         }
-        return res;
     }
-public:
+    
+    
+    
     int amountOfTime(TreeNode* root, int start) {
-         map<TreeNode * ,TreeNode * > mpp;
-        TreeNode * target = f(root,start,mpp);
-        map<TreeNode*,int>vis;
-        vis[target]=1;
+//          map<TreeNode * ,TreeNode * > mpp;
+//         TreeNode * target = f(root,start,mpp);
+//         map<TreeNode*,int>vis;
+//         vis[target]=1;
         
-        queue<TreeNode *>q;
-        q.push(target);
+//         queue<TreeNode *>q;
+//         q.push(target);
         
-        int lvl=0;
+//         int lvl=0;
         
-        while(!q.empty()){
-            int size=q.size();
-            int fl=0;
+//         while(!q.empty()){
+//             int size=q.size();
+//             int fl=0;
             
-            for(int i=0; i<size;i++){
-                TreeNode * node = q.front(); 
-                q.pop();
-                if(mpp[node] and !vis[mpp[node]]){
-                    fl=1;
-                    q.push(mpp[node]);
-                    vis[mpp[node]] = 1;
-                }
-                if(node->left and !vis[node->left]){
-                    fl=1;
-                    q.push(node->left);
-                    vis[node->left]=1;
-                }
-                if(node->right and !vis[node->right]){
-                    fl=1;
-                    q.push(node->right);
-                    vis[node->right]=1;
-                }
-            }
-            if(fl){
-                    lvl++;
-            }
-            }
-        return lvl;
+//             for(int i=0; i<size;i++){
+//                 TreeNode * node = q.front(); 
+//                 q.pop();
+//                 if(mpp[node] and !vis[mpp[node]]){
+//                     fl=1;
+//                     q.push(mpp[node]);
+//                     vis[mpp[node]] = 1;
+//                 }
+//                 if(node->left and !vis[node->left]){
+//                     fl=1;
+//                     q.push(node->left);
+//                     vis[node->left]=1;
+//                 }
+//                 if(node->right and !vis[node->right]){
+//                     fl=1;
+//                     q.push(node->right);
+//                     vis[node->right]=1;
+//                 }
+//             }
+//             if(fl){
+//                     lvl++;
+//             }
+//             }
+//         return lvl;
+        
+        
+        int n=1e5+1;
+        vector<int>graph[n];
+        traversal(root,graph,-1);
+        vector<int>depth(n,0);
+        dfs(start,graph,depth,-1);
+   return ans;
     }
 };
