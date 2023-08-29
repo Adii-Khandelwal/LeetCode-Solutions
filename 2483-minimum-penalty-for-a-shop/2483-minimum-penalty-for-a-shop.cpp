248f803fb31e7362a4379a33856c7ab548001fbe
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-08-29 10:48:32 +0530

    Time: 22 ms (85.33%), Space: 10.7 MB (86.10%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,37 +2,50 @@ class Solution {
 public:
     int bestClosingTime(string a) {
    
-        int s=a.size();
-			vector<int> y,n;
-			y.push_back(0);
-			n.push_back(0);
+//         int s=a.size();
+// 			vector<int> y,n;
+// 			y.push_back(0);
+// 			n.push_back(0);
 			
-        int cnt=0;
-			for(int i=0;i<s;i++)
-            {
-				if(a[i]=='N')cnt++;
-				n.push_back(cnt);
-			}
-			cnt=0;
+//         int cnt=0;
+// 			for(int i=0;i<s;i++)
+//             {
+// 				if(a[i]=='N')cnt++;
+// 				n.push_back(cnt);
+// 			}
+// 			cnt=0;
 			
-        for(int i=s-1;i>=0;i--)
-        {
-				if(a[i]=='Y')cnt++;
-				y.push_back(cnt);
-		}
-			reverse(y.begin(),y.end());
+//         for(int i=s-1;i>=0;i--)
+//         {
+// 				if(a[i]=='Y')cnt++;
+// 				y.push_back(cnt);
+// 		}
+// 			reverse(y.begin(),y.end());
 		
-        int ans=INT_MAX,ind=0;
-			for(int i=0;i<s+1;i++)
-            {
-				int h=y[i]+n[i];
-				if(h<ans)
-                {
-					ans=h;
-					ind=i;
-				}
-			}
-			return ind;
+//         int ans=INT_MAX,ind=0;
+// 			for(int i=0;i<s+1;i++)
+//             {
+// 				int h=y[i]+n[i];
+// 				if(h<ans)
+//                 {
+// 					ans=h;
+// 					ind=i;
+// 				}
+// 			}
+// 			return ind;
+        
+         int n=a.size();
+        int ind=-1,mxscore=0,score=0;
+        for(int i=0;i<n;i++){
+            if(a[i]=='Y') score++;
+            else score--;
+            if(score>mxscore){
+                mxscore=score;
+                ind=i;
+            }
+        }
+        return ind+1;
+        
 
     }
 };
