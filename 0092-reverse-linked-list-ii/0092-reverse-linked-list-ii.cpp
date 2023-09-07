d2d4ebdcb7a67e24e9967939221d26011234f592
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-09-07 14:51:53 +0530

    Time: 4 ms (38.20%), Space: 7.4 MB (89.74%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -22,8 +22,10 @@ public:
         ListNode *curr = prev->next; // curr pointer will be just after prev
         // reversing
         for(int i = 0; i < right - left; i++){
-            ListNode *forw = curr->next; // forw pointer will be after curr
-            curr->next = forw->next;
+            ListNode *forw = curr->next; 
+            // forw pointer will be after curr in every iteration
+            
+            curr->next = forw->next;//prev will be in one place only
             forw->next = prev->next;
             prev->next = forw;
         }
