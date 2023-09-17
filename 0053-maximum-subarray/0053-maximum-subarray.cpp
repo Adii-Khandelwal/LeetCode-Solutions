201f8fd427da7cf7e7de307af856090c4ce7b9c6
Adii-Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-09-17 19:27:05 +0530

    Time: 95 ms (23.63%), Space: 68 MB (7.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -36,10 +36,14 @@ public:
         int maxi=-1e9;
         for(int i=0;i<n;i++)
         {
-            sum+=nums[i];
+            // sum+=nums[i];
+            // maxi=max(maxi,sum);
+            // if(sum<0)
+            //     sum=0;
+            
+            sum=max(nums[i],sum+nums[i]);
             maxi=max(maxi,sum);
-            if(sum<0)
-                sum=0;
+            
         }
         return maxi;
         
