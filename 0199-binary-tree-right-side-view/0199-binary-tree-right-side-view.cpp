c590390acb5955824635d2a76708fbf6ba0d4629
Aditya Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-10-31 14:35:53 +0530

    Time: 3 ms (60.48%), Space: 12.2 MB (76.04%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,52 +11,53 @@
  */
 class Solution {
 public:
-//     vector<int> rightSideView(TreeNode* root) {
-//         vector<int> res;
-        
-//         recursion(root,0,res);
-//         return res;
-//     }
-    
-//     void recursion(TreeNode *root, int level, vector<int> &res)
-//     {
-//         if(!root)return;
-        
-//         if(res.size()==level)res.push_back(root->val);
-//         recursion(root->right,level+1,res);
-//         recursion(root->left,level+1,res);
-//     }
-    
-    vector<int> rsv(TreeNode * root)
-    {queue<TreeNode*> q;
-        vector<int>ans;
-        if(!root)
-            return ans;
-        q.push(root);
-//         insert a break statement lets say a null to denote that the level has ended
-        q.push(NULL);
-        while(!q.empty()){
-            TreeNode* current_node=q.front();
-            q.pop();
-            
-            if(current_node->left){
-                q.push(current_node->left);
-            }
-            if(current_node->right){
-                q.push(current_node->right);
-            }
-            if(q.front()==NULL)
-            {
-                q.pop();
-                ans.push_back(current_node->val);
-                if(!q.empty())
-                q.push(NULL);
-            }           
-        }
-        return ans;
-    }
-        
     vector<int> rightSideView(TreeNode* root) {
-        return rsv(root);
+        vector<int> res;
+        
+        recursion(root,0,res);
+        return res;
     }
+    
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(!root)return;
+        
+        if(res.size()==level)res.push_back(root->val);
+        recursion(root->right,level+1,res);
+        recursion(root->left,level+1,res);
+    }
+    
+//     vector<int> rsv(TreeNode * root)
+//     {
+//         queue<TreeNode*> q;
+//         vector<int>ans;
+//         if(!root)
+//             return ans;
+//         q.push(root);
+// //         insert a break statement lets say a null to denote that the level has ended
+//         q.push(NULL);
+//         while(!q.empty()){
+//             TreeNode* current_node=q.front();
+//             q.pop();
+            
+//             if(current_node->left){
+//                 q.push(current_node->left);
+//             }
+//             if(current_node->right){
+//                 q.push(current_node->right);
+//             }
+//             if(q.front()==NULL)
+//             {
+//                 q.pop();
+//                 ans.push_back(current_node->val);
+//                 if(!q.empty())
+//                 q.push(NULL);
+//             }           
+//         }
+//         return ans;
+//     }
+        
+//     vector<int> rightSideView(TreeNode* root) {
+//         return rsv(root);
+    // }
 };
