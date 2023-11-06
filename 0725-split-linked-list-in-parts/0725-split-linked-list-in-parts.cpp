2f0f3da9ab30725d59770bd410e5a767e229a87a
Aditya Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-11-06 12:34:46 +0530

    Time: 3 ms (94.13%), Space: 9.2 MB (14.31%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -15,16 +15,22 @@ public:
         int len = 0;
         for (ListNode* node = root; node; node = node->next)
             len++;
-        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
+        int n = len / k, r = len % k;
+        // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
+        
         ListNode* node = root, *prev = nullptr;
-        for (int i = 0; node && i < k; i++, r--) {
+        
+        for (int i = 0; node && i < k; i++, r--) 
+        {
             parts[i] = node;
-            for (int j = 0; j < n + (r > 0); j++) {
-                prev = node;
-                node = node->next;
-            }
+                for (int j = 0; j < n + (r > 0); j++) 
+                {
+                    prev = node;
+                    node = node->next;
+                }
             prev->next = nullptr;
         }
+        
         return parts;
     }
 };
