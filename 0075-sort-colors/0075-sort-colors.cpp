a3fa08c8d4a7fb6f7913351381e7fdfdaf1c05a7
Aditya Khandelwal <119524943+Adii-Khandelwal@users.noreply.github.com>
2023-11-10 23:31:14 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (33.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& A) {
        
-        int n=nums.size();
+        int n=A.size();
         
-        int zero=0,one=0,two=0;
+        // int zero=0,one=0,two=0;
         
-        for(int i=0;i<n;i++)
-        {
+//         for(int i=0;i<n;i++)
+//         {
             
-            if(nums[i]==0)
-                zero++; 
-            if(nums[i]==1)
-                one++;
-            if(nums[i]==2)
-                two++;
-        }
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
+//             if(nums[i]==0)
+//                 zero++; 
+//             if(nums[i]==1)
+//                 one++;
+//             if(nums[i]==2)
+//                 two++;
+//         }
+//         int cnt=0;
+//         for(int i=0;i<n;i++)
+//         {
             
-            if(cnt<zero)
-                nums[i]=0;
-            if(cnt >= zero && cnt<zero+one)
-                nums[i]=1;
+//             if(cnt<zero)
+//                 nums[i]=0;
+//             if(cnt >= zero && cnt<zero+one)
+//                 nums[i]=1;
             
-            if(cnt>=zero+one)
-                nums[i]=2;
-            cnt++;
-        }
-    
+//             if(cnt>=zero+one)
+//                 nums[i]=2;
+//             cnt++;
+//         }
+        
+       int second=n-1, zero=0;
+            for (int i=0; i<=second; i++) {
+                while (A[i]==2 && i<second) swap(A[i], A[second--]);
+                while (A[i]==0 && i>zero) swap(A[i], A[zero++]);
+            }
     
     
     
